e30f46d746e159f5fa658997c0dfbbd0c21536d4
adichnadra <adichandra019@gmail.com>
2021-05-12 15:08:26 +0700

    1.0 Membuat Splash Screen

---
+++ app/src/main/java/com/adichandra/yourselfapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.adichandra.yourselfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    Thread timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        img = findViewById(R.id.logo);
+        TextView textView = findViewById(R.id.appname);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+
+        textView.startAnimation(animation);
+        img.startAnimation(animation);
+
+        timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    synchronized (this){
+                        wait(3000);
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, WalkthroughActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
